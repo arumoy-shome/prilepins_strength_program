ecd5bdb74e324e6d20e4d0e3e21fb94bc398ef2a
arumoy-shome <arumoy.shome@gmail.com>
2015-02-20 18:41:52 -0500

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+  end
+
+  def create
+  end
+end
---
+++ db/migrate/20150220001252_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.integer :squat_max
+      t.integer :deadlift_max
+      t.integer :bench_max
+
+      t.timestamps null: false
+    end
+  end
+end
