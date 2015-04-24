aa914d63f1669fdd8e610540b7abfc4007533d24
arumoy-shome <arumoy.shome@gmail.com>
2015-04-23 22:51:10 -0400

    fake posts seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
 
-# create sample users
 Rake::Task['db:reset'].invoke
 admin = User.create!(email: "arumoy.shome@gmail.com",
   password: "secret00",
