363e5323f3d94c71d78ce5cc28dd2e0bcf64cc83
Arumoy Shome <arumoy.shome@gmail.com>
2015-04-26 21:05:57 -0400

    create and publish posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,28 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
+    @post = current_user.posts.new(posts_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to home_path, notice: 'Post was successfully published.' }
+      else
+        format.html { render 'static_pages/home' }
+      end
+    end
   end
 
   def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to user_posts_url, notice: 'Post was successfully destroyed.' }
+    end
   end
 
   private
 
   def posts_params
-    params.require(:posts).permit(:content)
+    params.require(:post).permit(:content)
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def home
+    if user_signed_in?
+      @post = current_user.posts.new
+      @posts = current_user.posts.paginate(page: params[:page])
+    end
   end
 
   def contact
